d9f4e103481327f2ac9ccbbf84958488d46aa147
Dan Eliahu <daneliyahu@cs.huji.ac.il>
2023-05-31 21:10:52 +0300

    Importing player sprites and Animating the player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,11 +7,22 @@ public class PlayerController : MonoBehaviour
     private PlayerControls _playerControls;
     private Vector2 _movement;
     private Rigidbody2D _rb;
+    private Animator _animator;
+    private SpriteRenderer _spriteRenderer;
+    private Camera _mainCam;
+    private static readonly int IsRunning = Animator.StringToHash("IsRunning");
 
     private void Awake()
     {
         _playerControls = new PlayerControls();
         _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        _mainCam = Camera.main;
     }
 
     private void OnEnable()
@@ -22,6 +33,7 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         PlayerInput();
+        AdjustPlayerFacingDirection();
     }
 
     private void FixedUpdate()
@@ -32,6 +44,15 @@ public class PlayerController : MonoBehaviour
     private void PlayerInput()
     {
         _movement = _playerControls.Movement.Move.ReadValue<Vector2>();
+        _animator.SetBool(IsRunning, _movement != Vector2.zero);
+    }
+
+    private void AdjustPlayerFacingDirection()
+    {
+        if (!_mainCam) return;
+
+        var mousePosition = _mainCam.ScreenToWorldPoint(Input.mousePosition);
+        _spriteRenderer.flipX = mousePosition.x < transform.position.x;
     }
 
     private void Move()
