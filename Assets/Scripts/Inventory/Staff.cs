40053faa36f7182300cbdd53593f502773e99fae
Dan Eliahu <daneliyahu@cs.huji.ac.il>
2023-07-06 21:54:56 +0300

    Magic Staff

--- Assets/Scripts/Inventory/Staff.cs
+++ Assets/Scripts/Inventory/Staff.cs
@@ -2,9 +2,25 @@ using UnityEngine;
 
 public class Staff : Weapon
 {
+    [SerializeField] private GameObject _magicLaser;
+    [SerializeField] private Transform _magicLaserSpawnPoint;
+
+    private Animator _animator;
+    private static readonly int Fire = Animator.StringToHash("Fire");
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+    
     public override void Attack()
     {
-        Debug.Log("Staff Attack");
+        _animator.SetTrigger(Fire);
+    }
+
+    public void SpawnStaffProjectileAnimEvent()
+    {
+        Instantiate(_magicLaser, _magicLaserSpawnPoint.position, Quaternion.identity);
     }
 
     private void Update()
